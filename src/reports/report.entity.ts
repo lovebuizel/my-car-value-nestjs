2e6a5b74a38cf0e324aa9a4fa94522a381808421
lovebuizel <40226745+lovebuizel@users.noreply.github.com>
2022-04-22 03:40:17 +0800

    add report entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,10 +1,28 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Report {
-    @PrimaryGeneratedColumn()
-    id: string;
+  @PrimaryGeneratedColumn()
+  id: string;
 
-    @Column()
-    price: number;
+  @Column()
+  price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
 }
